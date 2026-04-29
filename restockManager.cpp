acce6af72f4beed6e9026f2086933e72151c5bea
Kenshin Drona <kenshin.drona51@gmail.com>
2026-04-29 15:38:24 -0700

    Refactor RestockTask methods and add operator overload
    Refactor RestockTask class methods and constructors for clarity and consistency. Added operator overload for comparing urgency scores.

--- restockManager.cpp
+++ restockManager.cpp
@@ -1,30 +1,30 @@
 #include "restockManager.h"
 
-RestockTask::RestockTask(const std::string &id, const std::string &itemName, int quantityNeeded, int urgencyScore) {
+// --- RestockTask --- //
+RestockTask::RestockTask(const std::string &id, const std::string &itemName, int quantityNeeded, int urgencyScore)
+    : id(id), itemName(itemName), quantityNeeded(quantityNeeded), urgencyScore(urgencyScore) {}
 
+const std::string &RestockTask::getId() const { return id; }
+const std::string &RestockTask::getItemName() const { return itemName; }
+int RestockTask::getQuantityNeeded() const { return quantityNeeded; }
+int RestockTask::getUrgencyScore() const { return urgencyScore; }
+
+// Operator overlord for comparing urgencies in the priority queue.
+bool operator<(const RestockTask& a, const RestockTask& b) {
+    return a.getUrgencyScore() < b.getUrgencyScore();
 }
-const std::string &RestockTask::getId() const {
+// --- Restock Task End --- //
 
-}
-const std::string &RestockTask::getItemName() const {
+// --- Constructors and Destructos --- //
+RestockManager::RestockManager() {}
+RestockManager::~RestockManager() {}
+// --- Constructors and Destructors END --- //
 
-}
-int RestockTask::getQuantityNeeded() const {
-
-}
-int RestockTask::getUrgencyScore() const {
-
-}
-
-RestockManager::RestockManager() {
-
-}
-RestockManager::~RestockManager() {
-
-}
+// --- Restock Manager --- //
 bool RestockManager::isEmpty() const {
-
+  return restockQueue.empty();
 }
+
 int RestockManager::calculateUrgency(const Item &item, int targetQuantity) const {
 
 }
