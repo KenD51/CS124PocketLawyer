194e41a121a0b5f7992e627bf10382b599cac445
Darcie <dharxz.baculi101@gmail.com>
2026-04-28 23:48:54 -0700

    Add merge and merge_sort function declarations
    Added function declarations for merge and merge_sort.

---
+++ sortingAlgo.h
@@ -0,0 +1,15 @@
+
+/**
+ *  @brief Merges two adjacent rangs in an array (merge)
+ *  @param from; the start of the first range
+ *  @param mid; the end of the first range
+ *  @param to; the end of the second range
+ */
+void merge(int a[], int from, int mid, int to);
+
+/**
+ *  @brief sorts the elements in a range of an array
+ *  @param from; start o the range to sort
+ *  @param to; end of the range to sort
+ */
+void merge_sort(int a[], int from, int to);
